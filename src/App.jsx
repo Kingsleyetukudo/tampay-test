a3370e0450630031c76f2f5d653d5450d49dd3a1
kingsley etukudo <kingsleyj90@gmail.com>
2025-02-02 23:05:10 +0100

    vercel de

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />}>
             <Route index element={<DashboardDefault />} />
-            <Route path="/transaction" element={<Transactions />} />
+            <Route path="transaction" element={<Transactions />} />
           </Route>
         </Routes>
       </Router>

86c3242fbd5f3e47e342ac92f89d480fb77766be
kingsley etukudo <kingsleyj90@gmail.com>
2025-02-03 09:20:02 +0100

    netlify  depolyed12452

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import Dashboard from "./views/Dashboard";
 import DashboardDefault from "./views/DashboardDefault";
+import { PageTitleProvider } from "./components/PageTitleContext";
 
 const Transactions = lazy(() => import("./views/Transactions"));
 
 function App() {
   return (
-    <Router>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Dashboard />}>
-            <Route index element={<DashboardDefault />} />
-            <Route path="transactions" element={<Transactions />} />
-          </Route>
-        </Routes>
-      </Suspense>
-    </Router>
+    <PageTitleProvider>
+      <Router>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Dashboard />}>
+              <Route index element={<DashboardDefault />} />
+              <Route path="transactions" element={<Transactions />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </Router>
+    </PageTitleProvider>
   );
 }
 
